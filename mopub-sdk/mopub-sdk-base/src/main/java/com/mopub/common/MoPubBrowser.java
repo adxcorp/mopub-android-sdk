64b7f0de97341b14101fc48de4be7d4b75245c68
Chiung Choi <god@malangstudio.com>
2017-06-22 11:36:14 +0900

    [Modify] handling exception when setProgress occur exception

--- mopub-sdk/mopub-sdk-base/src/main/java/com/mopub/common/MoPubBrowser.java
+++ mopub-sdk/mopub-sdk-base/src/main/java/com/mopub/common/MoPubBrowser.java
@@ -111,10 +111,13 @@ public class MoPubBrowser extends Activity {
 
         mWebView.setWebChromeClient(new WebChromeClient() {
             public void onProgressChanged(WebView webView, int progress) {
-                setTitle("Loading...");
-                setProgress(progress * 100);
-                if (progress == 100) {
-                    setTitle(webView.getUrl());
+                try {
+                    setTitle("Loading...");
+                    setProgress(progress * 100);
+                    if (progress == 100) {
+                        setTitle(webView.getUrl());
+                    }
+                } catch(Exception e) {
                 }
             }
         });
