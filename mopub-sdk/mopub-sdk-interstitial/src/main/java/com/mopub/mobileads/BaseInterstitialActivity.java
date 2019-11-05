01c4021d0197246b5cb27d4ce1020c16534feef3
Chiung Choi <god@adxcorp.kr>
2019-11-05 11:43:42 +0900

    [Modify] remove immersive mode flags

--- mopub-sdk/mopub-sdk-interstitial/src/main/java/com/mopub/mobileads/BaseInterstitialActivity.java
+++ mopub-sdk/mopub-sdk-interstitial/src/main/java/com/mopub/mobileads/BaseInterstitialActivity.java
@@ -29,23 +29,6 @@ abstract class BaseInterstitialActivity extends Activity {
     public abstract View getAdView();
 
     @Override
-    protected void onResume() {
-        super.onResume();
-
-        int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            flags |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        }
-
-        mCloseableLayout.setSystemUiVisibility(flags);
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
